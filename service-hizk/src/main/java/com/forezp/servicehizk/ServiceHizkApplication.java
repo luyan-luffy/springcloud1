dad208524e6d0f1307b55571581822164111a65c
luyan <Administrator@DESKTOP-F41F9DT>
2019-07-22 16:39:16 +0800

    test cloud22

---
+++ service-hizk/src/main/java/com/forezp/servicehizk/ServiceHizkApplication.java
@@ -0,0 +1,49 @@
+package com.forezp.servicehizk;
+
+import brave.sampler.Sampler;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+@SpringBootApplication
+@RestController
+public class ServiceHizkApplication {
+
+    public static void main(String[] args) {
+
+        SpringApplication.run(ServiceHizkApplication.class, args);
+    }
+    private static final Logger LOG = Logger.getLogger(ServiceHizkApplication.class.getName());
+
+    @Autowired
+    private RestTemplate restTemplate;
+
+    @Bean
+    public RestTemplate getRestTemplate(){
+        return new RestTemplate();
+    }
+    @RequestMapping("/hi")
+    public String callHome(){
+        LOG.log(Level.INFO, "calling trace service-hi  ");
+        return restTemplate.getForObject("http://localhost:8989/miya", String.class);
+    }
+    @RequestMapping("/info")
+    public String info(){
+        LOG.log(Level.INFO, "calling trace service-hi ");
+
+        return "i'm service-hi";
+
+    }
+
+    @Bean
+    public Sampler defaultSampler() {
+        return Sampler.ALWAYS_SAMPLE;
+    }
+}
